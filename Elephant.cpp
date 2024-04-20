6a49dce0c16ddc46f6b76c1d0c9db205de7ec1f4
ajaditya00 <118525667+ajaditya00@users.noreply.github.com>
2024-04-20 22:39:40 +0530

    cpp
    Using Cpp

---
+++ Elephant.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    int steps = ceil(x / 5.0);
+    cout << steps << endl;
+    return 0;
+}
