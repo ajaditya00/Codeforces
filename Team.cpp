c0161473e95120f4457d0a83d1c391c328bedb2b
ajaditya00 <118525667+ajaditya00@users.noreply.github.com>
2024-04-23 19:52:52 +0530

    another way using cpp

--- Team.cpp
+++ Team.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+/*#include<bits/stdc++.h>
 using namespace std;
 
 int main(){
@@ -14,5 +14,37 @@ int main(){
     }
     }
     cout<<count;
+return 0;
+}*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int a,b,c;
+    int count=0;
+    int take=0;
+    for(int i=0;i<n;i++){
+        cin>>a>>b>>c;
+        if(a==1){
+            count++;
+        }
+         if(b==1){
+            count++;
+        }
+         if(c==1){
+            count++;
+        }
+         if(count>=2){
+            take++;
+            count=0;
+        }
+        else{
+            count=0;
+        }
+    }
+    cout<<take;
 return 0;
 }
